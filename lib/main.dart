72f27425794b266706764629d9cb10b074d7b787
T98f1 <100216046+T98f1@users.noreply.github.com>
2022-02-26 18:30:29 +0300

    الواجب الماصدف 25\2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
           body: SafeArea(
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
           Container(
             margin: EdgeInsets.all(20),
@@ -21,70 +22,126 @@ class MyApp extends StatelessWidget {
             ),
             child: ClipRRect(
                 borderRadius: BorderRadius.all(Radius.circular(20)),
-                child: Image.network(
-                    "https://i1.hespress.com/wp-content/uploads/2017/03/Temple_of_Heaven_613651700.jpg")),
+                child: Image.asset("aseets/images/tample.jpg")),
           ),
-          Text(
-            'Festival Chinatown',
-            textAlign: TextAlign.start,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 20.0,
+          Padding(
+            padding: const EdgeInsets.only(left: 20),
+            child: SizedBox(
+              width: double.infinity,
+              child: Text(
+                "Festival chinatown",
+                textAlign: TextAlign.start,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+              ),
             ),
           ),
           Container(
-            height: 2,
-            width: double.infinity,
-            margin: EdgeInsets.all(20),
-            color: Colors.grey[400],
+              width: double.infinity,
+              height: 2,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.grey[300])),
+          Row(children: [
+            Expanded(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 120),
+                    child: Text(
+                      "Name",
+                      style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          fontSize: 18,
+                          color: Colors.grey[300]),
+                      textAlign: TextAlign.start,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 15),
+                    child: Text("Michal Fernando",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.black)),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 130),
+                    child: Text("Date",
+                        style: TextStyle(
+                            fontWeight: FontWeight.normal,
+                            fontSize: 18,
+                            color: Colors.grey[300])),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 10),
+                    child: Text("12 February 2022",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.black)),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 60),
+                    child: Text("Date",
+                        style: TextStyle(
+                            fontWeight: FontWeight.normal,
+                            fontSize: 18,
+                            color: Colors.grey[300])),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 12),
+                    child: Text("16:00 PM",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.black)),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 60),
+                    child: Text("Seat",
+                        style: TextStyle(
+                            fontWeight: FontWeight.normal,
+                            fontSize: 18,
+                            color: Colors.grey[300])),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 5),
+                    child: Text("UNOJI B12",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.black)),
+                  ),
+                ],
+              ),
+            ),
+          ]),
+          Expanded(
+            child: Divider(
+              indent: 20,
+              endIndent: 20,
+              height: 10,
+              thickness: 2,
+              color: Colors.grey[300],
+            ),
           ),
-          Row(
-            //mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                flex: 2,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text ("Name",
-                        textAlign: TextAlign.start,
-                        style: TextStyle(
-                          fontWeight: FontWeight.normal,
-                          fontSize: 16,
-                            color: Colors.grey
-
-                    )
-                    ),
-                    Text("",
-                    )
-                  ],
-                ),
-              ),
-              Expanded(
-                flex: 1,
-                child: SizedBox(
-                  width: 22,
-                ),
-              ),
-              Expanded(
-                flex: 2,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text ("Date",
-                        style: TextStyle(
-                          fontWeight: FontWeight.normal,
-                          fontSize: 16,
-                          color: Colors.grey
-
-                        )
-                    ),
-                  ],
-                ),
-              )
-            ],
-          )
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.only(bottom: 50),
+            child: Image.asset("aseets/images/barcode.png"),
+          )),
         ],
       )
 
